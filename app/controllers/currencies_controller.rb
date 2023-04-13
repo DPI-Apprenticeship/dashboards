51081e3b5f45f6add743b7e5b754a013605d05ea
Meenoow Castillo <xcastillo33@gmail.com>
2023-04-13 22:58:05 +0000

    Rate resolved

--- app/controllers/currencies_controller.rb
+++ app/controllers/currencies_controller.rb
@@ -1,4 +1,6 @@
 class CurrenciesController < ApplicationController
+  require 'open-uri'
+  require 'json'
  def first_currency
   @raw_data = open("https://api.exchangerate.host/symbols").read
   @parsed_data = JSON.parse(@raw_data)
@@ -19,6 +21,27 @@ class CurrenciesController < ApplicationController
   @from_symbol = params.fetch("from_currency")
 
   render({ :template => "currency_templates/step_two.html.erb" })
+ end
+
+ def convert_currency
+  # {"from_currency"=>"ALL", "to_exchange"=>"AED"}
+  @from_symbol = params.fetch("from_currency")
+  @to_symbol = params.fetch("to_exchange")
+
+  api_url = "https://api.exchangerate.host/convert?from=#{@from_symbol}&to=#{@to_symbol}"
+
+  # TODO: replace USD and EUR with from_symbol and to_symbol
+  @raw_data = open(api_url).read
+  @parsed_data = JSON.parse(@raw_data)
+
+  # TODO: update with the parsed_data
+
+  #@exchange_hash = @parsed_data.fetch("convert")
+  #@rate = @exchange_hash.keys
+
+  @rate = @parsed_data.fetch("result")
+
+  render({ :template => "currency_templates/step_three.html.erb" })
  end 
  
 
